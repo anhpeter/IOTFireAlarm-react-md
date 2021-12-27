288f52e2b8c188dab46538799334f531b48b44b0
Peter Anh <webfullstack99@gmail.com>
2021-12-28 00:49:56 +0700

    update api domain

--- src/constants/app_constant.js
+++ src/constants/app_constant.js
@@ -2,5 +2,6 @@ export const IS_DEV = false;
 export const API_DOMAIN = IS_DEV
     ? 'http://localhost:5000'
     : 'https://it-fire-alarm.herokuapp.com';
+    
 
     export const LAST_REALTIME_QTY = 30;
