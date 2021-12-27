2dbcdc48907ff76bf5ff3cdc190ae16b490db905
Peter Anh <webfullstack99@gmail.com>
2021-12-27 08:47:13 +0700

    .

--- src/constants/app_constant.js
+++ src/constants/app_constant.js
@@ -1,6 +1,6 @@
-export const IS_DEV = true;
+export const IS_DEV = false;
 export const API_DOMAIN = IS_DEV
     ? 'http://localhost:5000'
-    : 'https://it3-fire-api.herokuapp.com';
+    : 'https://it-fire-alarm.herokuapp.com';
 
     export const LAST_REALTIME_QTY = 30;
